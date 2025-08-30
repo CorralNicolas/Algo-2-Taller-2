0f9986442ad1adca02dec721ad80b49525380590
Nicolas Corral <168582484+CorralNicolas@users.noreply.github.com>
2025-08-29 21:16:09 -0300

    Create Horario.java
    Nuevo

---
+++ Horario.java
@@ -0,0 +1,29 @@
+package aed;
+
+public class Horario {
+    private int hora;
+    private int minutos;
+    public Horario(int hora, int minutos) {
+        this.hora = hora;
+        this.minutos = minutos;
+    }
+
+    public int hora() {
+        return this.hora;
+    }
+
+    public int minutos() {
+        return this.minutos;
+    }
+
+    @Override
+    public String toString() {
+        return this.hora + ":" + this.minutos;
+    }
+
+    @Override
+    public boolean equals(Object otro) {
+        return otro != null && otro.getClass() == this.getClass() && this.hora == ((Horario)otro).hora && this.minutos == ((Horario)otro).minutos;
+    }
+
+}
